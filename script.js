833e6ab1b3d1bdc61cea57cd8fa1058d990f25f3
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-06-21 18:16:56 -0300

    refatorado lista de tarefas js

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const INPUT_TEXTO_TAREFA = document.getElementById('texto-tarefa');
 const OL_LISTA_TAREFAS = document.getElementById('lista-tarefas');
-const BTN_CRIAR_TAREFA = document.getElementById('criar-tarefa');
-const BTN_APAGA_TUDO = document.getElementById('apaga-tudo');
+const BTN_CRIAR_TAREFA = document.getElementById('adicionar-tarefa');
+const BTN_APAGA_TUDO = document.getElementById('remover-tudo');
 const BTN_REMOVER_FINALIZADOS = document.getElementById('remover-finalizados');
+let counter = 0;
 
 const completedTaskArray = [];
 let lastTargetSelected;
 
 function insertNewListItem() {
-  const NEW_LI = document.createElement('li');
-  NEW_LI.innerText = INPUT_TEXTO_TAREFA.value;
-  OL_LISTA_TAREFAS.appendChild(NEW_LI);
-  INPUT_TEXTO_TAREFA.value = '';
+  if (INPUT_TEXTO_TAREFA.value !== '') {
+    const NEW_LI = document.createElement('li');
+    OL_LISTA_TAREFAS.appendChild(NEW_LI);
+    counter += 1;
+
+    const NEW_CHECKBOX = document.createElement('input');
+    NEW_CHECKBOX.type = 'checkbox';
+    NEW_CHECKBOX.className = 'checkbox-tarefa';
+    NEW_CHECKBOX.id = counter;
+    NEW_LI.appendChild(NEW_CHECKBOX);
+
+    const NEW_LABEL = document.createElement('label');
+    NEW_LABEL.htmlFor = counter;
+    NEW_LABEL.innerHTML += INPUT_TEXTO_TAREFA.value;
+    NEW_LI.appendChild(NEW_LABEL);
+
+    INPUT_TEXTO_TAREFA.value = '';
+  }
 }
 
 function selectLi(event) {
